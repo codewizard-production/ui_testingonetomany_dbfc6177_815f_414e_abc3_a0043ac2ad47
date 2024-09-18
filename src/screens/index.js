23b55b1ff87ff2240d042ea6cd421b6acff11be9
Administrator <Administrator@10.0.10.244>
2024-09-18 14:23:46 +0000

    initial commit

---
+++ src/screens/index.js
@@ -0,0 +1,10 @@
+
+import StartupCreate from "./startup_oneToMany/startup_create.js";
+import StartupEdit from "./startup_oneToMany/startup_edit.js";
+import StartupView from "./startup_oneToMany/startup_view.js";
+import StartupTiles from "./startup_tiles/startup_tiles.js";
+
+export {
+StartupCreate, StartupEdit, StartupView, 
+StartupTiles
+};
